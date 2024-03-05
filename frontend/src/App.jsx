d514b1d13c372e329b58350dd64f43093513c9cd
TLG <tranlangiac@gmail.com>
2024-03-05 22:35:47 +0700

    Adding daisyUI into frontend side.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<>
+			<button className="btn btn-primary">One</button>
+			<button className="btn btn-secondary">Two</button>
+			<button className="btn btn-accent btn-outline">Three</button>
+		</>
+	);
 };
 
 export default App;
